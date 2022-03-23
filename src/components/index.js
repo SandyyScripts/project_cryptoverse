c14dd7c142da1a11bc307eebf346b0522772b428
Sandeep Swami <swami.2@iitj.ac.in>
2022-03-24 00:01:01 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line
+export { default as Homepage } from "./Homepage";
+// eslint-disable-next-line
+export { default as Navbar } from "./Navbar";
+// eslint-disable-next-line
+export { default as Cryptocurrencies } from "./Cryptocurrencies";
+// eslint-disable-next-line
+export { default as CryptoDetails } from "./CryptoDetails";
